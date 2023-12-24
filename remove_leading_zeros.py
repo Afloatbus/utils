1af5efd0004cf151eee9a78213aacf08fead62a6
William <91826947+Afloatbus@users.noreply.github.com>
2023-12-24 01:29:39 +0000

    Add files via upload

---
+++ remove_leading_zeros.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+def remove_leading_zeros(input_file, output_file, column_name):
+    """removes leading zeros from a csv file 
+
+    Args:
+        input_file (string): the file to remove zeroes from
+        output_file (string): the output file
+        column_name (string): the colum to remove the zeroes from
+    """
+    
+    df = pd.read_csv(input_file)
+    df[column_name] = df[column_name].str.lstrip('0')
+    df.to_csv(output_file, index=False)
+
+
+if __name__ == "__main__":
+    input_csv_file = "input_csv_file.csv"
+    output_csv_file = "output_csv_file.csv"
+    column_to_modify =  "column_to_modify.csv"
+    remove_leading_zeros(input_csv_file, output_csv_file, column_to_modify)
